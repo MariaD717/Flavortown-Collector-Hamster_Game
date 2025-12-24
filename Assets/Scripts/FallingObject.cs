d0dc0cfac1fe8f7d78d50393c266f881c28201db
MariaD717 <maria717nd@gmail.com>
2025-12-23 16:56:34 -0800

    Make Objects Randomly spawn&fall

--- Assets/Scripts/FallingObject.cs
+++ Assets/Scripts/FallingObject.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class FallingObject : MonoBehaviour
 {
+    public GameObject Tool;
+    private float timer = 0;
+    public float lowestPoint = -7;
+    public float highestPoint = 7;
+    public float spawnRate = 2;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+       
 
-    // Update is called once per frame
+    }
     void Update()
     {
         
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            spawnObject();
+        }
+    }
+
+    // Update is called once per frame
+    void spawnObject()
+    {
+
+        Instantiate(Tool, new Vector3(Random.Range(lowestPoint, highestPoint), transform.position.y, 0),transform.rotation);
+
     }
 }
